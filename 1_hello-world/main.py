b967f35633d11f4e9ac28b10a48b13d3db460617
Pawel Kiszczak <pawel.kiszczak@icloud.com>
2024-05-17 17:47:20 +0200

    local commit

---
+++ 1_hello-world/main.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from llama_index.core import VectorStoreIndex
+from llama_index.readers.web import SimpleWebPageReader
+
+def main(url: str) -> None:
+    documents = SimpleWebPageReader(html_to_text=True).load_data(urls=[url])
+    index = VectorStoreIndex.from_documents(documents=documents)
+    query_engine = index.as_query_engine()
+    response = query_engine.query("What is LlamaIndex?")
+    print(response)
+
+if __name__ == "__main__":
+    # Load the environment variables
+    load_dotenv()
+
+    print('Hello world LlamaIndex Course!')
+    print("\n*****\n")
+
+    url = "https://www.llamaindex.ai/"
+    main(url)
