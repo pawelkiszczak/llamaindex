8265810c802b4c08500cfe66ea4dbe54c4b99238
Pawel Kiszczak <pawel.kiszczak@icloud.com>
2024-05-19 14:00:31 +0200

    build ingestion pipeline

--- 1_hello-world/main.py
+++ 1_hello-world/main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from llama_index.core import VectorStoreIndex
 from llama_index.readers.web import SimpleWebPageReader
 
+
 def main(url: str) -> None:
     documents = SimpleWebPageReader(html_to_text=True).load_data(urls=[url])
     index = VectorStoreIndex.from_documents(documents=documents)
@@ -10,11 +11,12 @@ def main(url: str) -> None:
     response = query_engine.query("What is LlamaIndex?")
     print(response)
 
+
 if __name__ == "__main__":
     # Load the environment variables
     load_dotenv()
 
-    print('Hello world LlamaIndex Course!')
+    print("Hello world LlamaIndex Course!")
     print("\n*****\n")
 
     url = "https://www.llamaindex.ai/"
