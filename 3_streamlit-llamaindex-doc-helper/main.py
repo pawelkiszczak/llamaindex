0438e706a7aaab711dc76002670f41e1eb8a8d56
Pawel Kiszczak <pawel.kiszczak@icloud.com>
2024-05-20 20:06:07 +0200

    working ver with context lookup

--- 3_streamlit-llamaindex-doc-helper/main.py
+++ 3_streamlit-llamaindex-doc-helper/main.py
@@ -48,10 +48,12 @@ index = get_index()
 
 #####
 
-if 'chat_engine' not in st.session_state.keys():
-    st.session_state.chat_engine = index.as_chat_engine(chat_mode='context', verbose=True)
+# Add chat_engine to the st.session_state
+if 'chat_engine' not in st.session_state:
+    st.session_state.chat_engine = index.as_chat_engine(chat_mode='best', verbose=True, max_nodes=10, relevance_threshold=0.6)
 
-if 'messages' not in st.session_state.keys():
+# Add messages to the st.session_state
+if 'messages' not in st.session_state:
     st.session_state.messages = [
         {
             "role": "assistant",
@@ -59,21 +61,40 @@ if 'messages' not in st.session_state.keys():
         }
     ]
 
+# User input
 if prompt := st.chat_input("Your question here..."):
     st.session_state.messages.append({
         'role': 'user',
         'content': prompt
     })
 
+# Go through messages
 for message in st.session_state.messages:
     with st.chat_message(message['role']):
         st.markdown(message['content'])
 
+# Check for the last message in the session_state
 if st.session_state.messages[-1]['role'] != 'assistant':
     with st.chat_message('assistant'):
         with st.spinner('Thinking...'):
-            response = st.session_state.chat_engine.chat(message=prompt)
-            st.markdown(response.response)
+            response = st.session_state.chat_engine.stream_chat(message=prompt)
+
+            # Nodes to retreive context found
+            nodes = [node for node in response.source_nodes]
+
+            with st.expander("Check context"):
+                for idx, node in enumerate(nodes):
+                    st.text_area(label=f"Context {idx} - {node.node_id} - {node.score:.2f}",
+                                value=node.text,
+                                max_chars=5000)
+                    
+            with st.expander("Check context - column version"):
+                for col, node in zip(st.columns(len(nodes)), nodes):
+                    with col:
+                        st.header(f"Source node: {node.node_id} | Score: {node.score:.2f}")
+                        st.write(node.text)
+
+            st.write_stream(response.response_gen)
 
             message = {
                 'role': 'assistant',
